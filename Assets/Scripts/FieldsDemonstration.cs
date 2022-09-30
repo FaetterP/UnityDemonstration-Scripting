517ee83541a4730d79c0f98dd384f49927063d6b
FaetterP <56697273+FaetterP@users.noreply.github.com>
2022-09-30 16:21:02 +0300

    Добавлены поля в инспекторе

---
+++ Assets/Scripts/FieldsDemonstration.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class FieldsDemonstration : MonoBehaviour
+    {
+        [Header("Visible fields")]
+        [SerializeField] private int _int;
+        [SerializeField] private float _float;
+        [SerializeField] private bool _bool;
+        [SerializeField] [TextArea(2,5)] private string _string;
+        [SerializeField] private KeyCode _enum;
+        [Space] [Space] [Space]
+        [SerializeField] private GameObject GameObject;
+        [SerializeField] private Vector3 Vector3;
+        [SerializeField] private Vector2 Vector2;
+        [SerializeField] private AnimationCurve Curve;
+        [SerializeField] [Range(1, 5)] private int _range;
+
+        [Header("Invisible fields")]
+        [SerializeField] private const int _constInt = 3;
+        [SerializeField] private readonly int _readonlyInt;
+        [HideInInspector] public readonly int _hidden;
+    }
+}
--- Assets/Scripts/TestComponent.cs
+++ Assets/Scripts/TestComponent.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 
 public class TestComponent : MonoBehaviour
 {
+    [SerializeField] private int _speed;
+
     void Update()
     {
         float horizontalDirection = Input.GetAxis("Horizontal");
-        Vector3 anglesSpeed = new Vector3(0, 0, -30);
+        Vector3 anglesSpeed = new Vector3(0, 0, _speed);
         gameObject.transform.Rotate(horizontalDirection * Time.deltaTime * anglesSpeed);
     }
 }
