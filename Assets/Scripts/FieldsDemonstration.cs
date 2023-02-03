63cde6290d6dd35b76cadc9b02298bc7be0b124a
FaetterP <56697273+FaetterP@users.noreply.github.com>
2023-02-03 23:02:06 +0300

    Доделан FieldsDemonstration
    • Добавлены поля.
    • Показаны некоторые атрибуты.
    • Добавлена иконка для компонента.
    • Добавлены значения по умолчанию.

--- Assets/Scripts/FieldsDemonstration.cs
+++ Assets/Scripts/FieldsDemonstration.cs
@@ -14,12 +14,36 @@ namespace Assets.Scripts
         [SerializeField] private GameObject GameObject;
         [SerializeField] private Vector3 Vector3;
         [SerializeField] private Vector2 Vector2;
+        [SerializeField] private Quaternion _quaternion;
         [SerializeField] private AnimationCurve Curve;
         [SerializeField] [Range(1, 5)] private int _range;
+        [SerializeField] private Color _color;
+        [SerializeField] private Gradient _gradient;
 
         [Header("Invisible fields")]
         [SerializeField] private const int _constInt = 3;
         [SerializeField] private readonly int _readonlyInt;
-        [HideInInspector] public readonly int _hidden;
+        [HideInInspector] public int _hidden;
+
+        [Header("bool vs enum")]
+        [SerializeField] private bool _isControlledByKeyboardNotMouse;
+        enum ControllerType
+        {
+            [InspectorName("Override Keyboard Name")] Keyboard,
+            Mouse
+        }
+        [SerializeField] private ControllerType _controller;
+
+        [Header("Colors")]
+        [SerializeField] [ColorUsage(true)] private Color _colorWithAplha;
+        [SerializeField] [ColorUsage(true, true)] private Color _colorHDR;
+
+        [Header("Gradient")]
+        [SerializeField] [GradientUsage(true)] private Gradient _gradientHDR;        
+
+        [Header("default values")]
+        [SerializeField] private LayerMask _layerMask;
+        [SerializeField] private Transform _transform;
+        [SerializeField] private Sprite _sprite;
     }
 }
